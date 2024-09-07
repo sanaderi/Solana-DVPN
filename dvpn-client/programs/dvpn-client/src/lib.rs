4873f67d7594032ad56157c6753c7caa949b6f7a
Sadeq <sadeq.naderi@gmail.com>
2024-09-08 00:39:52 +0330

    Retrive price from pythnetwork, enable pda account for program, enable client pay sol to create account

--- dvpn-client/programs/dvpn-client/src/lib.rs
+++ dvpn-client/programs/dvpn-client/src/lib.rs
@@ -1,7 +1,9 @@
 use anchor_lang::prelude::*;
+use anchor_lang::system_program::{transfer, Transfer};
+use chrono::{Duration, Utc};
 use pyth_solana_receiver_sdk::price_update::{get_feed_id_from_hex, PriceUpdateV2};
 
-declare_id!("6ep5FBgrzidkqkqboodgHfYGVasqB6Z3BzF8uQQAcyjC");
+declare_id!("GuNu64rJ3eudKozZtgnCK5x1AwKEoge3d6pgFJYXXLTL");
 
 pub const MAXIMUM_AGE: u64 = 60; // One minute
 pub const FEED_ID: &str = "0xef0d8b6fda2ceba41da15d4095d1da392a0d2f8ed0c6c7bc0f4cfac8c280b56d"; // SOL/USD price feed id from https://pyth.network/developers/price-feed-ids
@@ -12,28 +14,50 @@ pub mod dvpn_client {
     use super::*;
 
     // Define the 'create_plan' function
-    pub fn create_plan(
-        ctx: Context<CreatePlan>,
-        title: String,
-        expiration_date: i64,
-    ) -> Result<()> {
-        let plan = &mut ctx.accounts.plan;
-        // plan.title = title;
-        msg!("program called");
+    pub fn create_plan(ctx: Context<CreatePlan>, expiration_date: i64) -> Result<()> {
+        //Retrieve solana price
         let price_update = &mut ctx.accounts.price_update;
         let price = price_update.get_price_no_older_than(
             &Clock::get()?,
             MAXIMUM_AGE,
             &get_feed_id_from_hex(FEED_ID)?,
         )?;
-        let price_string = price.price.to_string(); // Assuming `price` has a `price` field
 
-        plan.title = title;
-        plan.title = price_string;
+        // Calculate the precision factor
+        let base = 10_f64;
+        let expo = price.exponent as f64;
+        let recieve_price = price.price as f64;
+        let price_in_dollars = recieve_price * base.powf(expo);
+
+        let price_per_sec: f64 = 2.0 / price_in_dollars / 2592000_f64;
+        let account_price: f64 = price_per_sec * expiration_date as f64 * 86400_f64;
+        let fund_lamports: u64 = (account_price * 1000000000_f64).round() as u64;
+        let pda = &mut ctx.accounts.pda_account;
+        let signer = &mut ctx.accounts.user;
+        let system_program = &ctx.accounts.system_program;
+        let pda_balance_before = pda.get_lamports();
+
+        transfer(
+            CpiContext::new(
+                system_program.to_account_info(),
+                Transfer {
+                    from: signer.to_account_info(),
+                    to: pda.to_account_info(),
+                },
+            ),
+            fund_lamports,
+        )?;
+        let pda_balance_after = pda.get_lamports();
+
+        require_eq!(pda_balance_after, pda_balance_before + fund_lamports);
+
+        let plan: &mut Account<'_, _> = &mut ctx.accounts.plan;
 
         plan.owner = *ctx.accounts.user.key;
-        // plan.title = title;
-        plan.expiration_date = expiration_date;
+        let future_datetime = Utc::now() + Duration::days(expiration_date);
+        plan.expiration_date = future_datetime.timestamp();
+        // plan.expiration_date = fund_lamports as i64;
+
         Ok(())
     }
 }
@@ -48,20 +72,17 @@ pub struct CreatePlan<'info> {
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
     pub price_update: Account<'info, PriceUpdateV2>,
+    #[account(
+        mut,
+        seeds = [b"payment".as_ref()],
+        bump
+    )]
+    pub pda_account: SystemAccount<'info>,
 }
-
-// #[account(address = Pubkey::from_str(FEED_ID).unwrap() @ FeedError::InvalidPriceFeed)]
 
 // Define the 'Plan' account structure
 #[account]
 pub struct Plan {
     pub owner: Pubkey,
-    pub title: String,
     pub expiration_date: i64,
 }
-
-// #[error_code]
-// pub enum FeedError {
-//     #[msg("Invalid Price Feed")]
-//     InvalidPriceFeed,
-// }
